830e450c92b8c9389dc4310ad730369a3ad41eb0
rahulsingh237 <singh.rahul220499@gmail.com>
2020-05-30 03:36:12 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+import speech_recognition as sr
+from speak import *
+import os
+from PIL import Image
+
+r = sr.Recognizer()
+
+def speech():
+    with sr.Microphone() as source:
+        print('Say Something')
+        male("say something")
+        audio = r.listen(source)
+        text = r.recognize_google(audio)
+        print(text)
+        print("Did you say this (Yes/No) : ")
+        male("Did you say"+ text + "(please answer in Yes or No)")
+        a = r.listen(source)
+        answer = r.recognize_google(a)
+        if (answer == "Yes" or answer == "yes"):
+            print("Thanks, I've got it from here")
+            male("Thanks, I've got it from here")
+            try:
+                os.system('python get_hand.py -t "' + text + '" -s 4 -c 0,0,200' )
+                img = Image.open('images/0.png')
+                img.show()
+            except:
+                pass
+        else:
+            speech()
+
+def typing(text):
+    print("Thanks, I've got it from here")
+    male("Thanks, I've got it from here")
+    try:
+        os.system('python get_hand.py -t "' + text + '" -s 4 -c 0,0,200' )
+        img = Image.open('images/0.png')
+        img.show()
+    except:
+        pass
+
+
+      
+        
